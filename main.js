fe97e1ec59f553e483bcd9abdd7292c90dfd7ba8
Austinejnr <samuelamoto024@gmail.com>
2022-11-16 06:58:29 -0800

    started

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+new Swiper ("#swiper-1", {
+    effect: "fade",
+    autoplay: {
+        delay:2500,
+        disableOnInteraction: false
+    },
+    pagination: {
+            el: "#swiper-1 .swiper-pagination",
+            clickable: true
+    },
+    lazyLoading: true,
+    loop: true,
+    keyboard: {
+        enabled: true
+    },
+})
